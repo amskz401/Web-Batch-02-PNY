6bc92b49f90cb0bbe868844386423908064d342a
amskz401 <amskz401@gmail.com>
2023-10-24 15:45:36 +0500

    css basics

---
+++ more-about-css_2023-10-24/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Exploring CSS</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body class="index-page">
+	<header>
+		My Profile
+	</header>
+		
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-left">
+				
+					<img src="assets/images/profile-image.avif" width="100%">
+				
+				<p class="element-exp">
+					<b>Name: </b> John Doe
+				</p>
+				<p>
+					<b>Email: </b> john@email.com
+				</p>
+				<p>
+					<b>Phone: </b> +92 1234 4356677
+				</p>
+				<p>
+					<b>Skype: </b> my@sykpe
+				</p>
+			</div>
+			<div class="col-right">
+				<h1>My Profile Description</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<p>
+			Copyrights@PNY - 2023
+		</p>
+	</footer>
+</body>
+</html>
