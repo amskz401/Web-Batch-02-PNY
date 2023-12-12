7e71425adbcc9c557a4315c9e730a73e1d9a474c
amskz401 <amskz401@gmail.com>
2023-12-12 15:56:14 +0500

    php starter

---
+++ php-starter/index.php
@@ -0,0 +1,134 @@
+<?php 
+
+define("NAMEMMM", 12);
+// $_1 = 12;
+// $_2 = 12;
+// $_3 = 12;
+// $a1 = 1;
+// $a2 = 2;
+// $A1 = 43;
+// echo $a1;
+// echo NAMEMMM;
+
+// $scope = 12;
+
+// echo $scope;
+
+
+// function sc() {
+// 	// global $scope;
+
+// 	echo $GLOBALS['scope'];
+// }
+// sc();
+
+// $num = 12;
+
+
+// $str = "string";
+// var_dump($str);
+
+// $flag = true;
+
+// $num = 12.21;
+
+// $value = null;
+
+// $students = ['std1', 'std2'];
+// $students = array('std1', 'std2');
+
+
+// echo pi();
+
+// echo min(1,0, -1);
+
+// echo max(12,32,76,8);
+
+// echo round(1.5);
+
+// echo rand();
+// echo rand(100, 9999);
+
+// echo ceil(1.1);
+// echo floor(1.9);
+
+// $a = 10;
+// $b = (int) '32';
+
+// var_dump($b);
+
+// $c = $a + $b;
+
+// echo $c;
+
+// $a = 12;
+// $b = "12";
+// var_dump($a > $b);
+// echo $a++;
+// echo '<br>';
+// echo $a;
+
+// var_dump( 1 == 1 AND 2 == 3 );
+// var_dump( 1 == 1 or 2 == 3 );
+
+// $a = 1;
+// $b = 2;
+
+// if( $b = $a ) {
+// 	echo 'here';
+// }
+// echo '<br>';
+// echo $b;
+
+
+// warning;
+// fetal;
+// syntax;
+// logical;
+
+// echo 1;
+
+// try {
+// 	$conn = new mysqli("locl");
+// } catch (Exception $e) {
+// 	echo $e->getMessage();
+// }
+
+// echo '<br>';
+// echo 2;
+
+
+$a = 3;
+
+if($a == 1) {
+	echo 'in condition one';
+} 
+elseif($a == 2) {
+	echo 'in condition 2';
+} 
+elseif($a == 3) {
+	echo 'in condition 3';
+} 
+else {
+	echo 'default case';
+}
+
+switch ($a) {
+	case 1:
+		echo 'in condition one';
+		break;
+	case 2: 
+		echo 'in condition 2';
+		break;
+	case 3: 
+		echo 'in condition 3';
+		break;
+	default:
+		echo 'default case';
+		break;
+}
+
+
+
+
+ ?>
